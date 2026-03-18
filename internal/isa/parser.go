8e56557693ee4587ce6c57933a0e8e1da2283f8d
Dmitry Anokhin <anokhindm@gmail.com>
2026-03-18 17:31:18 +0300

    Refactor ParseLine and ruleMatchesTokens functions for improved error handling and clarity

--- internal/isa/parser.go
+++ internal/isa/parser.go
@@ -14,46 +14,58 @@ func ParseLine(line string) (Instruction, error) {
 		return Instruction{}, fmt.Errorf("Token expected")
 	}
 
-	var expected uint8
+	var expectedToken uint8
+	var errorToken uint8
 	var matched bool
+	var matchedRule Rule
 	for _, rule := range syntaxRules {
 		if tokens[0].Type == rule.Syntax[0] {
-			matched, expected = ruleMatchesTokens(rule, tokens)
+			matched, expectedToken, errorToken = ruleMatchesTokens(rule, tokens)
 			if matched {
+				matchedRule = rule
 				break
 			}
 		}
 	}
 	if !matched {
-		return Instruction{}, fmt.Errorf("Syntax error: expected %s, got %s", GetTokenTypeString(expected), tokens[0])
+		return Instruction{}, fmt.Errorf("Syntax error: expected %s, got %s", GetTokenTypeString(expectedToken), errorToken)
 	}
-	return ParseInstruction(syntaxRules[0], tokens)
+	return ParseInstruction(matchedRule, tokens)
 }
 
-func ruleMatchesTokens(rule Rule, tokens []Token) (bool, uint8) {
-	if len(rule.Syntax) != len(tokens) {
-		return false, 0
-	}
+func ruleMatchesTokens(rule Rule, tokens []Token) (bool, uint8, uint8) {
 	for i, tokenType := range rule.Syntax {
 		if tokens[i].Type != tokenType {
-			return false, tokenType
+			return false, tokenType, tokens[i].Type
 		}
 	}
-	return true, 0
+	return true, 0, 0
+}
+
+func ParseRegister(token string) (uint8, uint8, error) {
+	reg, found := GetRegisterNumber(token)
+	if !found {
+		return 0, 0, fmt.Errorf("Invalid register: %s", token)
+	}
+	if reg > 7 {
+		return reg - 7, 1, nil
+	} else {
+		return reg, 0, nil
+	}
 }
 
 func ParseInstruction(rule Rule, tokens []Token) (Instruction, error) {
 	switch rule.Type {
 	case RULE_ALU_REG_REG:
-		rd, found := GetRegisterNumber(tokens[1].TokenString)
-		if !found {
-			return Instruction{}, fmt.Errorf("Invalid register: %s", tokens[1].TokenString)
+		rd, bankd, err := ParseRegister(tokens[1].TokenString)
+		if err != nil {
+			return Instruction{}, err
 		}
-		rs, found := GetRegisterNumber(tokens[3].TokenString)
-		if !found {
-			return Instruction{}, fmt.Errorf("Invalid register: %s", tokens[3].TokenString)
+		rs, banks, err := ParseRegister(tokens[3].TokenString)
+		if err != nil {
+			return Instruction{}, err
 		}
-		return Instruction{Opcode: rule.Opcode, Rd: rd, Rs: rs}, nil
+		return Instruction{Opcode: rule.Opcode, Rd: rd, Rs: rs, Ex: bankd<<1 | banks}, nil
 	case RULE_LD_REG_REG:
 		rd, found := GetRegisterNumber(tokens[1].TokenString)
 		if !found {
